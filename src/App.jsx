7f5768b13270207800dff6cee1a75314f3699219
Gabrielle Asante <asante.gabrielle@gmail.com>
2025-03-09 13:35:38 +0000

    Adjusted container width and modified button positioning in TodoForm for different screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import todoLogo from "./assets/logo/todo-logo.svg";
 function App() {
   return (
     <div className="flex justify-center items-center h-screen m-auto">
-      <div className="flex flex-col bg-white h-[75%] w-[85%] rounded-lg shadow-lg p-6 lg:h-[80%]">
+      <div className="flex flex-col bg-white h-[75%] w-[85%] rounded-lg shadow-lg p-6 lg:h-[80%] lg:w-[70%]">
         <div className="flex flex-col justify-center">
           <a href="/" className="m-auto">
             {" "}
